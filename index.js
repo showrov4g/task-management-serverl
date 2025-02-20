b69c8b22963f0e19160dbdc78a58faac122fd3c2
showrov4g <showrovg00@gmail.com>
2025-02-20 23:31:13 +0600

    Database connection setup

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// Import the express module
-const express = require('express');
-const cors = require('cors')
-require('dotenv').config()
-const port =process.env.PORT || 3000;
+const express = require("express");
+const http = require ("http");
+const {Server} = require("socket.io")
+const cors = require("cors");
 const app = express();
+const server = http.createServer(app);
+require('dotenv').config();
+const port = process.env.PORT || 5000
+const io = new Server(server,{
+  cors:{origin:'http://localhost:5173', methods: ['GET','POST','PUT','DELETE']}
+})
 
-// middleware
-app.use(cors)
-app.use(express.json())
+//middlewares
+app.use(cors());
+app.use(express.json());
 
-
-// mongodb
-
+// database connection 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.23lvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,6 +33,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    // ----------------
     // my code here 
 
 
@@ -48,12 +52,11 @@ run().catch(console.dir);
 
 
 
-// Define a simple route
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.get('/', (req,res)=>{
+  res.send("server is running")
+})
+
+server.listen(port,()=>{
+  console.log("server in running on port", port)
+})
