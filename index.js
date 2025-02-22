578b8e91d6e72c64d0273e736574bdea00497226
showrov4g <showrovg00@gmail.com>
2025-02-22 13:40:49 +0600

    some error fixed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const functions = require("firebase-functions");
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const http = require("http");
@@ -89,3 +90,4 @@ app.post("/users", async (req, res) => {
 });
 
 server.listen(5000, () => console.log("Server running on port 5000"));
+exports.api = functions.https.onRequest(app);
