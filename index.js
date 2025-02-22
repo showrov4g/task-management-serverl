ccdd8890fca9245934df926bfcb45bc36b565cdd
showrov4g <showrovg00@gmail.com>
2025-02-22 06:38:30 +0600

    first commit

--- index.js
+++ index.js
@@ -1,110 +1,66 @@
 const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+
 const app = express();
 const server = http.createServer(app);
-require("dotenv").config();
-const port = process.env.PORT || 5000;
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
+  cors: { origin: "http://localhost:5173", methods: ["GET", "POST", "PUT", "DELETE"] },
 });
 
-//middlewares
 app.use(cors());
 app.use(express.json());
 
-// database connection
+// MongoDB Connection
+const uri = "mongodb+srv://task:kxF19z6agajmz6oj@cluster0.23lvn.mongodb.net";
+const client = new MongoClient(uri);
+let tasks, users;
 
-const {
-  MongoClient,
-  ServerApiVersion,
-  ConnectionCheckOutStartedEvent,
-  ObjectId,
-} = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.23lvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const connectDB = async () => {
+  await client.connect();
+  const db = client.db("taskdb");
+  tasks = db.collection("tasks");
+  users = db.collection("users");
+  console.log("Connected to MongoDB");
+};
+connectDB();
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+// Get Tasks for Logged-in User
+app.get("/tasks", async (req, res) => {
+  const { email } = req.query;
+  res.json(await tasks.find({ email }).toArray());
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // ----------------
-    // my code here
-    const taskCollection = client.db("taskList").collection("task");
-    const userCollection = client.db("taskList").collection("users");
-    // add user data to the database
-    // user data store api 
-    app.post('/users', async(req, res)=>{
-      const {userid, email, displayName} = req.body;
-      const user = {userid, email, displayName}
-      const existingUser = await userCollection.findOne({email})
-      if(!existingUser){
-        await userCollection.insertOne(user)
-      }
-      res.sendStatus(200);
-    })
-
-  //  get task data 
-  app.get('/tasks', async(req,res)=>{
-    const {email} = req.query;
-    const result = await taskCollection.find({email}).toArray();
-    res.send(result)
-  }) 
-
-  // add task 
-  app.post('/tasks', async(req,res)=>{
-    const task = req.body;
-    await taskCollection.insertOne(task)
-    io.emit("taskUpdate")
-    res.sendStatus(201)
-  })
-
-  // update task 
-  app.put("/tasks/:id", async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    await taskCollection.updateOne(query,{$set: req.body})
-    io.emit("taskUpdated")
-    res.sendStatus(200)
-  })
-    // delete task 
-  app.delete("/tasks/:id", async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    await taskCollection.deleteOne(query)
-    io.emit("taskUpdated")
-    res.sendStatus(200)
-  })
-
-
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("server is running");
+// Add Task
+app.post("/tasks", async (req, res) => {
+  await tasks.insertOne(req.body);
+  io.emit("taskUpdated");
+  res.sendStatus(201);
 });
 
-server.listen(port, () => {
-  console.log("server in running on port", port);
+// Update Task
+app.put("/tasks/:id", async (req, res) => {
+  await tasks.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+  io.emit("taskUpdated");
+  res.sendStatus(200);
 });
+
+// Delete Task
+app.delete("/tasks/:id", async (req, res) => {
+  await tasks.deleteOne({ _id: new ObjectId(req.params.id) });
+  io.emit("taskUpdated");
+  res.sendStatus(200);
+});
+
+// Store User Data
+app.post("/users", async (req, res) => {
+  const { userid, email, displayName } = req.body;
+  const existingUser = await users.findOne({ email });
+  if (!existingUser) await users.insertOne({ userid, email, displayName });
+  res.sendStatus(200);
+});
+
+server.listen(5000, () => console.log("Server running on port 5000"));
+ 
